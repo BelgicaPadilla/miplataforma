d4d8c6eada99b62992b029f1e316797b15052cc6
BelgicaPadilla <belgica.barre@utelvt.edu.ec>
2022-09-08 14:29:09 -0500

    base de datos

---
+++ datos.sql
@@ -0,0 +1,3 @@
+creata database miplataforma;
+use miplataforma;
+create table persona (idpersona int, nombres varchar(20), apellidos varchar(20), cedula varchar(10), idgenero int, fechanacimiento date, idnacionalidad int, idetnia int, idestadocivil int);
